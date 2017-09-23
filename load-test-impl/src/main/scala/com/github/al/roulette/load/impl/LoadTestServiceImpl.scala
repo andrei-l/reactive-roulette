0e116f7fa308d006402e130a110cc67ec2a46d4f
Andrei Loginov <loginov.andrei@gmail.com>
2017-09-23 02:55:49 +0100

    - slightly improved load test impl - now there won't a lot of uncontrolled errors

--- load-test-impl/src/main/scala/com/github/al/roulette/load/impl/LoadTestServiceImpl.scala
+++ load-test-impl/src/main/scala/com/github/al/roulette/load/impl/LoadTestServiceImpl.scala
@@ -2,33 +2,33 @@ package com.github.al.roulette.load.impl
 
 import java.time.Duration
 import java.util.UUID
+import java.util.concurrent._
 import java.util.concurrent.atomic.AtomicInteger
 
-import akka.NotUsed
 import akka.actor.Scheduler
 import akka.http.javadsl.model.headers
-import akka.stream.scaladsl.Source
-import com.github.al.roulette.bet
+import akka.stream.scaladsl.{Flow, Source}
+import akka.{Done, NotUsed}
 import com.github.al.roulette.bet.api.{Bet, BetService}
-import com.github.al.roulette.game.api.{Game, GameId, GameService}
+import com.github.al.roulette.game.api.{Game, GameEvent, GameId, GameService}
 import com.github.al.roulette.load.api.{LoadTestParameters, LoadTestService}
 import com.github.al.roulette.load.impl.FutureExtension._
 import com.github.al.roulette.player.api._
 import com.github.al.roulette.winnings.api.WinningsService
+import com.github.al.roulette.{bet, game}
 import com.lightbend.lagom.scaladsl.api.ServiceCall
 import com.lightbend.lagom.scaladsl.pubsub.{PubSubRegistry, TopicId}
 
+import scala.collection.immutable
 import scala.concurrent.duration.DurationDouble
 import scala.concurrent.{ExecutionContext, Future}
 import scala.language.{implicitConversions, postfixOps}
 import scala.util.{Random, Try}
 
-class LoadTestServiceImpl(gameService: GameService,
-                          betService: BetService,
-                          playerService: PlayerService,
-                          winningsService: WinningsService,
-                          pubSubRegistry: PubSubRegistry,
-                          scheduler: Scheduler)(implicit executionContext: ExecutionContext)
+class LoadTestServiceImpl(gameService: GameService, betService: BetService,
+                          playerService: PlayerService, winningsService: WinningsService,
+                          pubSubRegistry: PubSubRegistry, scheduler: Scheduler)
+                         (implicit executionContext: ExecutionContext)
   extends LoadTestService {
   private final val GameDefaultDuration = Duration.ofSeconds(10)
   private lazy val loadTestEventsTopic = pubSubRegistry.refFor(TopicId[LoadTestEvent])
@@ -36,6 +36,16 @@ class LoadTestServiceImpl(gameService: GameService,
 
   private final val PlayersCounter = new AtomicInteger(0)
   private final val GamesCounter = new AtomicInteger(0)
+
+  private final val GamesToPlay = new ArrayBlockingQueue[UUID](50)
+  private final val FinishedGames = ConcurrentHashMap.newKeySet[UUID]()
+
+  gameService.gameEvents.subscribe.atLeastOnce(Flow[GameEvent].map {
+    case e: game.api.GameStarted => GamesToPlay.add(e.gameId); Done
+    case e: game.api.GameFinished => FinishedGames.add(e.gameId); Done
+    case _ => Done
+  })
+
 
   override def startLoadTest: ServiceCall[LoadTestParameters, Source[String, NotUsed]] = {
     ServiceCall { parameters =>
@@ -45,12 +55,16 @@ class LoadTestServiceImpl(gameService: GameService,
   }
 
   private def startLoadTest(parameters: LoadTestParameters): Unit = {
-    loadTestEventsTopic.publish(LoadTestEvent(s"Load test started for next parameters: $parameters"))
+    loadTestEventsTopic.publish(LoadTestEvent(s"Load test started with next parameters: $parameters"))
 
-    val playerIdsFuturesSequence = PlayersCounter.get() until PlayersCounter.addAndGet(parameters.numberOfPlayers) map {
-      playerName => s"$playerName" -> playerService.registerPlayer.invoke(Player(s"$playerName"))
-    }
+    val playerIds: IndexedSeq[(String, Future[PlayerId])] = createPlayers(parameters.numberOfPlayers)
+    val playerIdsWithAccessToken: Future[IndexedSeq[(PlayerId, PlayerAccessToken)]] = loginPlayers(playerIds)
+    val gameIds: Future[IndexedSeq[GameId]] = createGames(parameters.numberOfConcurrentGames)
 
+    startPlacingBets(playerIdsWithAccessToken, gameIds, parameters.numberOfBetsToPlace)
+  }
+
+  private def loginPlayers(playerIdsFuturesSequence: IndexedSeq[(String, Future[PlayerId])]) = {
     val playerIdToAccessTokenSequenceOfFutureTries = for {
       (playerName, playerIdFuture) <- playerIdsFuturesSequence
       accessTokenFuture = playerService.login.invoke(PlayerCredentials(playerName))
@@ -60,32 +74,49 @@ class LoadTestServiceImpl(gameService: GameService,
     val playerIdToAccessTokenFutureTriesSequence = Future.sequence(playerIdToAccessTokenSequenceOfFutureTries)
     val playerIdToAccessTokenFutureSequence = playerIdToAccessTokenFutureTriesSequence.getSuccessfulFutures(enqueueMsg("Successfully created and logged in a user"))
     playerIdToAccessTokenFutureTriesSequence.forAllFailureFutures(msg => enqueueMsg(s"Failed to create and login a user:$msg"))
+    playerIdToAccessTokenFutureSequence
+  }
 
+  private def createPlayers(numberOfPlayers: Int): IndexedSeq[(String, Future[PlayerId])] = {
+    val playerIdsFuturesSequence = PlayersCounter.get() until numberOfPlayers map {
+      playerName => s"$playerName" -> playerService.registerPlayer.invoke(Player(s"$playerName"))
+    }
+    playerIdsFuturesSequence
+  }
+
+  private def createGames(numberOfConcurrentGames: Int) = {
     val gameIdsSequenceOfFutureTries = for {
-      gameName <- GamesCounter.get() until GamesCounter.addAndGet(parameters.numberOfConcurrentGames)
+      gameName <- GamesCounter.get() until GamesCounter.addAndGet(numberOfConcurrentGames)
       gameIdFuture = gameService.createGame.invoke(Game(s"$gameName", GameDefaultDuration))
     } yield gameIdFuture.toFutureTry
+
     val gameIdsFutureTriesSequence = Future.sequence(gameIdsSequenceOfFutureTries)
     val gameIdsFutureSequence = gameIdsFutureTriesSequence.getSuccessfulFutures(enqueueMsg("Successfully created a game"))
     gameIdsFutureTriesSequence.forAllFailureFutures(msg => enqueueMsg(s"Failed to create a game:$msg"))
+    gameIdsFutureSequence
+  }
 
-
+  private def startPlacingBets(playerIdToAccessTokenFutureSequence: Future[IndexedSeq[(PlayerId, PlayerAccessToken)]],
+                               gameIds: Future[IndexedSeq[GameId]],
+                               numberOfBetsToPlace: Int) = {
     val betsFuture = for {
-      gameIdsSequence <- gameIdsFutureSequence
+//      _ <- gameIds
       playerIdsToAccessTokensSequence <- playerIdToAccessTokenFutureSequence
-      bets <- placeBets(gameIdsSequence, playerIdsToAccessTokensSequence, parameters.numberOfBetsToPlace)
+      bets <- placeBets(playerIdsToAccessTokensSequence, numberOfBetsToPlace)
     } yield bets
 
     val betsSequence = betsFuture.getSuccessfulFutures(publishEvent("Bet has been successfully put"))
     betsFuture.forAllFailureFutures(msg => enqueueMsg(s"Failed to put a bet:$msg"))
   }
 
-  private def placeBets(gameIds: Seq[GameId], playerIdsToAccessTokens: Seq[(PlayerId, PlayerAccessToken)], numberOfBets: Int): Future[IndexedSeq[Try[NotUsed]]] = {
+  private def placeBets(playerIdsToAccessTokens: Seq[(PlayerId, PlayerAccessToken)], numberOfBets: Int): Future[IndexedSeq[Try[NotUsed]]] = {
     val bets = for {
-      _ <- 0 to Random.nextInt(numberOfBets)
-      gameId = gameIds(Random.nextInt(gameIds.length))
+      _ <- 0 to numberOfBets
+      gameId = GamesToPlay.poll(15, TimeUnit.SECONDS)
       (playerId, accessToken) = playerIdsToAccessTokens(Random.nextInt(playerIdsToAccessTokens.length))
-      bet = placeBet(gameId.gameId, playerId.playerId, accessToken.token)
+      bet = placeBet(gameId, playerId.playerId, accessToken.token)
+      if !FinishedGames.contains(gameId)
+      _ = GamesToPlay.add(gameId)
     } yield bet.toFutureTry
 
     Future.sequence(bets)
